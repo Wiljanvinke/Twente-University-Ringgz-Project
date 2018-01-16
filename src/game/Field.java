49a0711b95c6ceded17ff72d8529157f5df4fc29
bezem <bezem@BezemLaptop17>
2018-01-16 15:12:57 +0100

    Created new color class and inproved board and enums

--- src/game/Field.java
+++ src/game/Field.java
@@ -7,5 +7,19 @@ package game;
  * @version 0.1
  */
 public class Field {
-
+	
+	// variables
+	private Ring[] rings;
+	
+	/**
+	 * Creates an empty field.
+	 * Fields have 5 variables: the 4 ring sizes and a 5th field which determines if the field is occupied by a base
+	 */
+	public Field() {
+		rings = new Ring[5];
+		for (int i = 0; i < 5; i++) {
+			rings[i] = Ring.EMPTY;
+		}
+	}
+	
 }
