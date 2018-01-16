577acf0fd1092f06e19111cee937989d7cf99737
bezem <bezem@BezemLaptop17>
2018-01-16 16:02:56 +0100

    Finishing design of classes

---
+++ src/game/Size.java
@@ -0,0 +1,15 @@
+package game;
+
+/**
+ * A single ring size, each ring has 5 possible states:
+ * It has one of four possible sizes:
+ * Tiny, small, medium and large
+ * The fifth option is used for bases
+ * @author Wouter Bezemer
+ * @author Wiljan Vinke
+ * @version 0.1
+ */
+public enum Size {
+	// variables
+	TINY, SMALL, MEDIUM, LARGE, BASE;
+}
