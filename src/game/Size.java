1f07e19fc2274388a60b7df81211b590f7991e1f
bezem <bezem@BezemLaptop17>
2018-01-17 14:41:51 +0100

    Finished Color and Size methods and javadoc

--- src/game/Size.java
+++ src/game/Size.java
@@ -1,30 +1,41 @@
 package game;
 
 /**
- * A single ring size, each ring has 5 possible states:
+ * A single ring size, each ring has 5 possible states.
  * It has one of four possible sizes:
  * Tiny, small, medium and large
  * The fifth option is used for bases
  * @author Wouter Bezemer
  * @author Wiljan Vinke
- * @version 0.1
+ * @version 0.2
  */
 public enum Size {
 	TINY, SMALL, MEDIUM, LARGE, BASE;
 	
+	/**
+	 * Changes the size into an integer representing that size.
+	 * The smallest size has the smallest number.
+	 * @return 0 if tiny, 1 if small, 2 if medium, 3 if large and 4 if base
+	 */
 	public int toInt() {
-		int number;
+		int number = -1;
 		switch (this) {
 			case TINY: number = 0; break;
 			case SMALL: number = 1; break;
 			case MEDIUM: number = 2; break;
 			case LARGE: number = 3; break;
 			case BASE: number = 4; break;
-			default: number = -1; break;
 		}
 		return number;
 	}
 
+	/**
+	 * Changes a number into the appropriate size.
+	 * The smallest number returns the smallest base.
+	 * This method returns null if the number isn't between 0 and 4.
+	 * @param a number representing the size
+	 * @return TINY if 0, SMALL if 1, MEDIUM if 2, LARGE if 3, BASE if 4 else null
+	 */
 	public Size toEnum(int number) {
 		Size size;
 		switch (number) {
@@ -33,7 +44,7 @@ public enum Size {
 			case 2: size = MEDIUM; break;
 			case 3: size = LARGE; break;
 			case 4: size = BASE; break;
-			default: size = null; break;
+			default: size = null; break; // Make sure to check for this!
 		}
 		return size;
 	}
