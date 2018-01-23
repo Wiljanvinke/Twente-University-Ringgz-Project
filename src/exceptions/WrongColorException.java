1466efeff2207ede017dc0cb3fb05daf6a7254a8
bezem <bezem@BezemLaptop17>
2018-01-23 14:56:56 +0100

    Added exceptions and improved placeRing in field

---
+++ src/exceptions/WrongColorException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class WrongColorException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage() {
+		return "Error: That is not a valid color";
+	}
+}
---
+++ src/exceptions/WrongSizeException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class WrongSizeException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage() {
+		return "Error: That is not a valid size";
+	}
+}
