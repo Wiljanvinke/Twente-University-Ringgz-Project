98cde5c82d717c439a1d533289da59a6e81413a0
bezem <bezem@BezemLaptop17>
2018-01-24 15:06:59 +0100

    Test how long value calculation takes, started ComputerPlayer

--- src/players/ComputerPlayer.java
+++ src/players/ComputerPlayer.java
@@ -13,9 +13,29 @@ public class ComputerPlayer extends Player {
 	}
 
 	@Override
-	public int[] determineMove(Board board) {
-		// TODO Auto-generated method stub
-		return null;
+	public String determineMove() {
+		int highestValue;
+		int bestIndex;
+		int bestColor;
+		int bestSize;
+		getBoard().calculateValue(this);
+		// for each field
+		for (int i = 0; i < 25; i++) {
+			// for each color this player has
+			for (int j = 0; j < this.getColors().length; j++) {
+				// for each ring on this field
+				for (int w = 0; w < 5; w++) {
+					getBoard().getField(i).getValue(Color.toEnum(j), Size.toEnum(w));
+				}
+			}
+			
+			
+		}
+		
+		
+		// hier zet je de zetten om in String volgens het protocol.
+		
+		return "";
 	}
 
 }
