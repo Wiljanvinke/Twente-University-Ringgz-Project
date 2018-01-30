14ca19fc2afe8ff4a9a45338a2ac6a66f71e9ab4
Wiljan <Wiljan@Wiljan-PC.home>
2018-01-30 02:49:09 +0100

    New implementation structure for server/client

---
+++ src/servernew/Client.java
@@ -0,0 +1,164 @@
+package servernew;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import extra.Protocol;
+import extra.Protocol.Extension;
+
+
+/**
+ * Client class for a simple client-server application.
+ * @author  Theo Ruys
+ * @version 2005.02.21
+ */
+public class Client extends Thread {
+	private static final String USAGE = "usage: java server.Client "
+			+ "<name> <address> <port> <extention1> <extention2> <extention3>";
+
+	/** Start een Client-applicatie op. */
+	public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println(USAGE);
+            System.exit(0);
+        }
+
+		
+		InetAddress host = null;
+		int port = 0;
+    	Extension[] extensions = null;
+
+		try {
+			host = InetAddress.getByName(args[1]);
+		} catch (UnknownHostException e) {
+			print("ERROR: no valid hostname!");
+			System.exit(0);
+		}
+
+		try {
+			port = Integer.parseInt(args[2]);
+		} catch (NumberFormatException e) {
+			print("ERROR: no valid portnummer!");
+			System.exit(0);
+		}	
+
+        extensions = new Extension[args.length - 3];
+    	for (int i = 0; i < args.length - 3; i++) {
+        	Extension ext = null;
+        	switch (args[i + 3]) {
+    			case Protocol.CHAT: ext = Protocol.Extension.CHAT; break;
+    			case Protocol.CHALLENGE: ext = Protocol.Extension.CHALLENGE; break;
+    			case Protocol.LEADERBOARD: ext = Protocol.Extension.LEADERBOARD; break;
+    		}
+    		extensions[i] = ext;      	
+        }
+		try {
+			Client client = new Client(args[0], host, port, extensions);
+			client.sendMessage(args[0]);
+			client.start();
+			
+			do {
+				String input = readString("");
+				client.sendMessage(input);
+			} while (true);
+			
+		} catch (IOException e) {
+			print("ERROR: couldn't construct a client object!");
+			System.exit(0);
+		}
+
+	}
+	
+	private String clientName;
+	private Socket sock;
+	private BufferedReader in;
+	private BufferedWriter out;
+    protected Extension[] extensions;
+	public static final String EXIT = "exit";
+
+
+	/**
+	 * Constructs a Client-object and tries to make a socket connection.
+	 */
+	public Client(String name, InetAddress host, int port, Extension[] ext) throws IOException {
+
+		clientName = name;
+		sock = new Socket(host, port);
+		if (ext != null) {
+			this.extensions = ext;
+		}
+		this.in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+		this.out = new BufferedWriter(new OutputStreamWriter(sock.getOutputStream()));
+
+	}
+
+	/**
+	 * Reads the messages in the socket connection. Each message will
+	 * be forwarded to the MessageUI
+	 */
+	public void run() {
+		String input;
+		try {
+			while (true) {
+				input = in.readLine();
+				System.out.println(input);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			this.shutdown();
+		}
+	}
+
+	/** send a message to a ClientHandler. */
+	public void sendMessage(String msg) {
+		try {
+			out.write(msg + "\n");
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+			this.shutdown();
+		}
+	}
+
+	/** close the socket connection. */
+	public void shutdown() {
+		print("Closing socket connection...");
+		try {
+			in.close();
+			out.close();
+			sock.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			System.exit(0);
+		}
+	}
+
+	/** returns the client name. */
+	public String getClientName() {
+		return clientName;
+	}
+	
+	private static void print(String message) {
+		System.out.println(message);
+	}
+	
+	public static String readString(String tekst) {
+		System.out.print(tekst);
+		String antw = null;
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					System.in));
+			antw = in.readLine();
+		} catch (IOException e) {
+		}
+
+		return (antw == null) ? "" : antw;
+	}
+}
---
+++ src/servernew/Server.java
@@ -0,0 +1,120 @@
+package servernew;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import extra.Protocol;
+import extra.Protocol.Extension;
+
+/**
+ * Server. 
+ * @author  Theo Ruys
+ * @version 2005.02.21
+ */
+public class Server {
+    private static final String USAGE
+            = "usage: " + Server.class.getName() + " <port> <extention1> <extention2> <extention3>";
+
+    /** Start een Server-applicatie op. */
+    public static void main(String[] args) {
+        if (args.length > 4 || args.length == 0) {
+            System.out.println(USAGE);
+            System.exit(0);
+        }
+        
+    	Extension[] extensions = new Extension[args.length - 1];
+    	for (int i = 0; i < args.length - 1; i++) {
+        	Extension ext = null;
+        	switch (args[i + 1]) {
+    			case Protocol.CHAT: ext = Protocol.Extension.CHAT; break;
+    			case Protocol.CHALLENGE: ext = Protocol.Extension.CHALLENGE; break;
+    			case Protocol.LEADERBOARD: ext = Protocol.Extension.LEADERBOARD; break;
+    		}
+    		extensions[i] = ext;      	
+        }
+        Server server = new Server(Integer.parseInt(args[0]), extensions);
+
+        server.run();
+        
+    }
+
+
+    private int port;
+    private List<ClientHandler> threads;
+    private Extension[] extensions;
+    /** Constructs a new Server object. */
+    public Server(int portArg, Extension[] extArg) {
+    	port = portArg;
+    	extensions = extArg;
+    	threads = new ArrayList<ClientHandler>();
+    }
+    
+    /**
+     * Listens to a port of this Server if there are any Clients that 
+     * would like to connect. For every new socket connection a new
+     * ClientHandler thread is started that takes care of the further
+     * communication with the Client.
+     */
+    public void run() {
+    	Socket socket = null;
+    	ServerSocket ssocket = null;
+        try {
+        	ssocket = new ServerSocket(port);
+        	while (true) {
+        		socket = ssocket.accept();
+        		ClientHandler client = new ClientHandler(this, socket);
+        		addHandler(client);
+        	}
+        } catch (SocketException exc) {
+        	exc.printStackTrace();
+        } catch (IOException e) {
+        	e.printStackTrace();
+        } finally {
+        	if (ssocket != null) {
+        		try {
+        			ssocket.close();
+        		} catch (IOException e) {
+        			e.printStackTrace();
+        		}
+        	}
+        }
+    }
+    
+    public void print(String message) {
+        System.out.println(message);
+    }
+    
+    /**
+     * Sends a message using the collection of connected ClientHandlers
+     * to all connected Clients.
+     * @param msg message that is send
+     */
+    public void broadcast(String msg) {
+    	for (int i = 0; i < threads.size(); i++) {
+    		threads.get(i).sendMessage(msg);
+    	}
+		print(msg);
+    }
+    
+    /**
+     * Add a ClientHandler to the collection of ClientHandlers.
+     * @param handler ClientHandler that will be added
+     */
+    public void addHandler(ClientHandler handler) {
+    	threads.add(handler);
+		handler.start();
+    }
+    
+    /**
+     * Remove a ClientHandler from the collection of ClientHanlders. 
+     * @param handler ClientHandler that will be removed
+     */
+	public void removeHandler(ClientHandler handler) {
+		threads.remove(handler);
+	}
+}
